a4842e570915f1de54df87f379961c6c9d074fc6
Илья Казютин <gorgutz1@yandex.ru>
2020-06-04 12:27:10 +0300

    Добавьте файлы проекта.

---
+++ PolynomLib/Polynom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PolynomLib
+{
+    public class Polynom
+    {
+        private readonly double[] _a;
+
+        public Polynom(double[] a)
+        {
+            _a = a;
+        }
+
+        public double Value(double x)
+        {
+            var len = _a.Length;
+            if (len == 0) return double.NaN;
+            var y = _a[_a.Length - 1];
+            for (var n = len - 2; n >= 0; n--)
+                y = y * x + _a[n];
+            return y;
+        }
+
+        public double Value2(double x)
+        {
+            var y = 0d;
+            for (var n = 0; n < _a.Length; n++)
+                y += x * Math.Pow(x, n);
+
+            return y;
+        }
+    }
+}
---
+++ PolynomUnitTesting/Program.cs
@@ -0,0 +1,21 @@
+﻿using PolynomLib;
+using System;
+
+namespace PolynomUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = { 7, 5, 3 };
+            var p = new Polynom(a);
+
+            var y0 = p.Value(0);
+            var y1 = p.Value(1);
+            var y5 = p.Value(5);
+
+            Console.WriteLine("Fin.");
+            Console.ReadLine();
+        }
+    }
+}
